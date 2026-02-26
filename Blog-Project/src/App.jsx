fdf7e56e74e27423c90854014a3524101f7b2b6e
Tijedele <abiolatijesunimi02@gmail.com>
2026-02-26 14:47:10 +0100

    Fix case sensitive import path

--- Blog-Project/src/App.jsx
+++ Blog-Project/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/home/Home";
 import NewHome from "./pages/home/NewHome";
 import Signup from "./pages/signup/Signup";
 import Signin from "./pages/signin/Signin";
-import Single from "./pages/single/single";
+import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import UserSettings from "./pages/UserSettings/UserSettings";
 import SinglePost from "./pages/SinglePost/SinglePost";
@@ -22,12 +22,12 @@ export default function App() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/" element={<Signin />} />
       <Route path="/single/:post_id" element={<Single />} />
-      <Route path="/createpost" element={<Write/>} />
-      <Route path="/editpost/:post_id" element={<EditPost/>} />
+      <Route path="/createpost" element={<Write />} />
+      <Route path="/editpost/:post_id" element={<EditPost />} />
       {/* <Route path="/editpost/:post_id" element={<Edit/>} /> */}
-      <Route path="/blogs" element={<Blogs/>} />
-      <Route path="/usersettings" element={<UserSettings/>} />
-      <Route path="/profile" element={<UserProfile/>} />
+      <Route path="/blogs" element={<Blogs />} />
+      <Route path="/usersettings" element={<UserSettings />} />
+      <Route path="/profile" element={<UserProfile />} />
       {/* <Route path="/postdetails/:post_id" element={<SinglePost/>} /> */}
     </Routes>
   );
